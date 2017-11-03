eefe6e51afcb6281b06df6c67e87dbab05c8b949
Hanbyul Kim <hahanbyul@gmail.com>
2017-11-03 15:48:06 +0900

    [ADD] SeamCarver.java: removeVerticalSeam

--- Seam Carving/SeamCarver.java
+++ Seam Carving/SeamCarver.java
@@ -2,35 +2,28 @@ import edu.princeton.cs.algs4.Picture;
 import java.awt.Color;
 
 public class SeamCarver {
-    private Picture picture;
+    private Picture picture, working;
+    int H, W;
     private double[][] energy;
     private double[][] distTo;
     private int[][]    edgeTo;
     
     public SeamCarver(Picture picture) {               // create a seam carver object based on the given picture
-        this.picture = picture;
+        this.picture = picture;                        // original image
+        this.working = new Picture(picture);           // picture copy
 
-        this.energy = new double[width()][height()];
-        this.distTo = new double[width()][height()];
-        this.edgeTo = new int[width()][height()];
+        H = picture.height();
+        W = picture.width();
 
-        for (int row = 0; row < height(); row++) {
-            for (int col = 0; col < width(); col++) {
+        this.energy = new double[W][H];
+        this.distTo = new double[W][H];
+        this.edgeTo = new int[W][H];
+
+        for (int row = 0; row < H; row++) {
+            for (int col = 0; col < W; col++) {
                 energy[col][row] = energy(col, row);
                 distTo[col][row] = Double.POSITIVE_INFINITY;
             }
-        }
-    }
-
-    public void printDistTo() {
-        printArrary(distTo);
-    }
-
-    private void printArrary(double[][] array) {
-        for (int row = 0; row < height(); row++) {
-            for (int col = 0; col < width(); col++)
-                System.out.print(String.format("%10.1f ", array[col][row]));
-            System.out.println();
         }
     }
 
@@ -47,7 +40,7 @@ public class SeamCarver {
         }
     }
 
-    private void findShortest() {
+    private void updateDistTo() {
         for (int row = 0; row < height(); row++) {
             for (int col = 0; col < width(); col++) {
                 if (row == 0) distTo[col][row] = 1000;
@@ -61,37 +54,40 @@ public class SeamCarver {
     }
 
     public     int width() {                           // width of current picture
-        return this.picture.width();
+        return W;
     }
     
     public     int height() {                           // height of current picture
-        return this.picture.height();
+        return H;
     }
     
     public  double energy(int x, int y) {               // energy of pixel at column x and row y
-        if (x == 0 || x == width()-1 || y == 0 || y == height()-1) return 1000;
+        if (x < 0 || x >= width() || y < 0 || y >= height())
+            throw new IndexOutOfBoundsException();
+
+        if (x == 0 || x == width()-1 || y == 0 || y == height()-1)  // border condition
+            return 1000;
         return Math.sqrt(deltaXSq(x, y) + deltaYSq(x, y));
     }
 
     private double deltaXSq(int x, int y) {
-        Color left  = this.picture.get(x-1, y);
-        Color right = this.picture.get(x+1, y);
+        Color left  = working.get(x-1, y);
+        Color right = working.get(x+1, y);
 
         return Math.pow(left.getRed() - right.getRed(), 2) + Math.pow(left.getGreen() - right.getGreen(), 2) + Math.pow(left.getBlue() - right.getBlue(), 2);
     }
 
     private double deltaYSq(int x, int y) {
-        Color left  = this.picture.get(x, y-1);
-        Color right = this.picture.get(x, y+1);
+        Color left  = working.get(x, y-1);
+        Color right = working.get(x, y+1);
 
         return Math.pow(left.getRed() - right.getRed(), 2) + Math.pow(left.getGreen() - right.getGreen(), 2) + Math.pow(left.getBlue() - right.getBlue(), 2);
     }
 
     public   int[] findVerticalSeam() {                 // sequence of indices for vertical seam
-        int[] path = new int[height()];
+        updateDistTo();
 
-        findShortest();
-        printDistTo();
+        // find min val at the bottom line
         double min_val = Double.POSITIVE_INFINITY;
         int min_col = -1;
         for (int col = 0; col < width(); col++) {
@@ -100,7 +96,9 @@ public class SeamCarver {
                 min_col = col;
             }
         }
-        System.out.println(min_col);
+
+        // reconstruct path
+        int[] path = new int[height()];
         path[height()-1] = min_col;
         for (int row = height()-2; row >= 0; row--) {
             path[row] = path[row+1] - edgeTo[path[row+1]][row+1];
@@ -109,26 +107,98 @@ public class SeamCarver {
         return path;
     }
 
+    public    void removeVerticalSeam(int[] seam) {     // remove vertical seam from current picture
+        W--;
+
+        // shift working image
+        for (int row = 0; row < height(); row++)
+            for (int col = seam[row]; col < width(); col++)
+                working.set(col, row, working.get(col+1, row));
+
+        // update and shift energy array
+        for (int row = 0; row < height(); row++) {
+            if (seam[row] > 0) 
+                energy[seam[row]-1][row] = energy(seam[row]-1, row);
+            if (seam[row] <= width()-1)
+                energy[seam[row]][row] = energy(seam[row], row);
+            for (int col = seam[row]+1; col < width()-1; col++)
+                energy[col][row] = energy[col+1][row];
+            energy[width()-1][row] = 1000;
+        }
+
+        // initialize distTo array
+        for (int row = 0; row < H; row++) {
+            for (int col = 0; col < W; col++) {
+                distTo[col][row] = Double.POSITIVE_INFINITY;
+            }
+        }
+    }
+
     /*
     public   int[] findHorizontalSeam() {               // sequence of indices for horizontal seam
         return 0;
     }
     public    void removeHorizontalSeam(int[] seam) {   // remove horizontal seam from current picture
     }
-    public    void removeVerticalSeam(int[] seam) {     // remove vertical seam from current picture
-
-    }
     */
+
+    public void printWorking() {
+        System.out.println("working: ");
+        for (int row = 0; row < height(); row++) {
+            for (int col = 0; col < width(); col++)
+                System.out.print(String.format("%5d ", working.get(col, row).getRGB()));
+            System.out.println();
+        }
+    }
+
+    private void printPicture() {
+        System.out.println("picture: ");
+        for (int row = 0; row < picture.height(); row++) {
+            for (int col = 0; col < picture.width(); col++)
+                System.out.print(String.format("%5d ", picture.get(col, row).getRGB()));
+            System.out.println();
+        }
+    }
+
+    public void printDistTo() {
+        System.out.println("distTo: ");
+        printArrary(distTo);
+    }
+
+    public void printEnergy() {
+        System.out.println("energy: ");
+        printArrary(energy);
+    }
+
+    private void printArrary(double[][] array) {
+        for (int row = 0; row < height(); row++) {
+            for (int col = 0; col < width(); col++)
+                System.out.print(String.format("%10.1f ", array[col][row]));
+            System.out.println();
+        }
+    }
     
     public static void main(String[] args) {
-        Picture picture = new Picture(args[0]);
+        // Picture picture = new Picture(args[0]);
+        Picture picture = new Picture("seam/5x6.png");
         SeamCarver sc = new SeamCarver(picture);
         
         System.out.printf("%d-by-%d\n", sc.width(), sc.height());
         // picture.show();
+        /*
         int[] path = sc.findVerticalSeam();
         for (int i = 0; i < sc.height(); i++)
             System.out.print(String.format("%d ", path[i]));
-
+        */
+        /*
+        sc.printWorking();
+        int[] seam = new int[]{0, 0, 1, 0};
+        sc.removeVerticalSeam(seam);
+        sc.printWorking();
+        */
+        sc.printArrary(sc.energy);
+        int[] seam = new int[]{0, 1, 2, 3, 4, 3};
+        sc.removeVerticalSeam(seam);
+        sc.printArrary(sc.energy);
     }
 }
